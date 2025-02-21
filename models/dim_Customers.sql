bfe1811dc5cc1cab243be37211fb1db5860161d7
peter.johannessen@columbusglobal.com <peter.johannessen@columbusglobal.com>
2025-02-21 13:43:39 +0000

    6

---
+++ models/dim_Customers.sql
@@ -0,0 +1,25 @@
+
+
+
+
+{{ config(materialized='table') }}
+
+with Dim_Customers as (
+
+SELECT      [CustomerKey],
+			[WWICustomerID],
+			[Customer],
+			[BillToCustomer],
+			[Category],
+			[BuyingGroup],
+			[PrimaryContact],
+			[PostalCode],
+			[ValidFrom],
+			[ValidTo],
+			[LineageKey]
+FROM [dbt_Demo1].[dbo].[Customers]
+
+)
+
+select *
+from Dim_Customers
