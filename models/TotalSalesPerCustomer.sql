6ffd8f8bd9b84ffe142896fc90dbf4ebd0b539e3
Peter Johannessen <peter.johannessen@columbusglobal.com>
2025-02-27 10:35:03 +0000

    a

---
+++ models/TotalSalesPerCustomer.sql
@@ -0,0 +1,60 @@
+{{ config(
+    materialized='table',
+    schema="Gold"
+) }}
+
+with Customers as (
+
+SELECT      [CustomerKey],
+			[WWICustomerID],
+			[Customer],
+			[BillToCustomer],
+			[Category],
+			[BuyingGroup],
+			[PrimaryContact],
+			[PostalCode],
+			[ValidFrom],
+			[ValidTo],
+			[LineageKey]
+FROM {{ref("dim_Customers")}}
+
+), 
+
+
+Sales as (
+SELECT  
+            [SaleKey],
+			[CityKey],
+			[CustomerKey],
+			[BillToCustomerKey],
+			[StockItemKey],
+			[InvoiceDateKey],
+			[DeliveryDateKey],
+			[SalespersonKey],
+			[WWIInvoiceID],
+			[Description],
+			[Package],
+			[Quantity],
+			[UnitPrice],
+			[TaxRate],
+			[TotalExcludingTax],
+			[TaxAmount],
+			[Profit],
+			[TotalIncludingTax],
+			[TotalDryItems],
+			[TotalChillerItems],
+			[LineageKey]
+FROM {{ref("fact_sales")}}
+
+)
+
+
+select 
+Customers.Customer,
+SUM(Sales.TotalIncludingTax)
+from Customers 
+
+Left Join Sales on Customers.CustomerKey = Sales.CustomerKey
+
+GROUP BY Customers.Customer
+Order by SUM(Sales.TotalIncludingTax) Desc
